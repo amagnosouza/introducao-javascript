55a5fb23d5c15ec15f7c5a053f3823f80f3dedb0
amagnosouza <alexandremagnofs@gmail.com>
2020-03-26 09:59:24 -0300

    introdução js

---
+++ js/principal.js
@@ -0,0 +1,3 @@
+// console.log(document.querySelector("h1")); || Imprime somente o que foi selecionado
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista Maluca";
