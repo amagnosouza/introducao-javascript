f8299694f875f5ae84fdacec3fdc37a87cadd4aa
amagnosouza <alexandremagnofs@gmail.com>
2020-04-08 11:40:27 -0300

    Quebrando arquivos

---
+++ js/form.js
@@ -0,0 +1,50 @@
+// Escutar eventos
+
+// funcao anonima
+// titulo.addEventListener("click", function(){
+//     console.log("Aqui também funciona!");
+// });
+
+// function mostraMensagem(){
+//     console.log("Olá eu fui clicado!");
+// }
+
+// Escutar o botao de adicionar do fomulario.
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    //colocar os dados dentro das td's
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    // adicionar cada td dentro da tr
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    // adicionar os pacientes na tabela
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+});
+
+//event.preventDefault() previne o comportamento padrão de um formulario que é enviar os dados para outra pagina.
